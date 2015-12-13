5e6adfe6bbda124217e2ab0526dc068a890c8b29
Harshal <harshal.carpenter@gmail.com>
2015-12-13 12:34:14 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,38 +1,38 @@
 (function(){
 	var app = angular.module('myApp',[]);
 	app.controller('AppController',['$scope','$http',function($scope,$http){
-	var refresh = function(){
-	$http.get('/contactlist').success(function(response){
-		console.log("Yes we did");
-		$scope.contactlist = response;
-	});};
-	refresh();
-		
-	$scope.addContact = function(){
-		$http.post('/contactlist',$scope.cont).success(
-		function(response){console.log(response);});
-		console.log($scope.cont);
+		var refresh = function(){
+		$http.get('/contactlist').success(function(response){
+			console.log("Yes we did");
+			$scope.contactlist = response;
+		});};
 		refresh();
-	};
-	
-	$scope.remove = function(id){
-		console.log(id);
-		$http.delete('/contactlist/'+id);
-		refresh();
-	}
-
-	$scope.edit = function(id){
-		console.log(id);
-		$http.get('/contactlist/'+id).success(function(response){$scope.cont = response;});
-	};
-	$scope.update = function(){
-		console.log($scope.cont._id);
-		$http.put('/contactlist/'+$scope.cont._id, $scope.cont).success(function(resp){
+			
+		$scope.addContact = function(){
+			$http.post('/contactlist',$scope.cont).success(
+			function(response){console.log(response);});
+			console.log($scope.cont);
 			refresh();
-		});
-	}
-	$scope.deselect = function(){
-		$scope.cont = "";
-	}
+		};
+		
+		$scope.remove = function(id){
+			console.log(id);
+			$http.delete('/contactlist/'+id);
+			refresh();
+		}
+	
+		$scope.edit = function(id){
+			console.log(id);
+			$http.get('/contactlist/'+id).success(function(response){$scope.cont = response;});
+		};
+		$scope.update = function(){
+			console.log($scope.cont._id);
+			$http.put('/contactlist/'+$scope.cont._id, $scope.cont).success(function(resp){
+				refresh();
+			});
+		}
+		$scope.deselect = function(){
+			$scope.cont = "";
+		}
 	}]);
 })();
